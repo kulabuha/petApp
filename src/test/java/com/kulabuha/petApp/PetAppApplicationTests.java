67ff613d74d79b1e74ba5a5e91aea9f9c7bd91db
aleksei.kukabukhov@telekom.com <aleksei.kukabukhov@telekom.com>
2023-01-30 17:45:53 +0200

    init commit

---
+++ src/test/java/com/kulabuha/petApp/PetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kulabuha.petApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
